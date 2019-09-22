56c5e6fcdccc2a7ea7fdb5a0533b12f8093199dc
Khoa <ngockhoatran1996@gmail.com>
2019-09-22 23:46:27 +0700

    remove unusual import

--- dummy/src/main/java/com/hellodoctor/dummy/services/PropertiesMapper.java
+++ dummy/src/main/java/com/hellodoctor/dummy/services/PropertiesMapper.java
@@ -1,6 +1,5 @@
 package com.hellodoctor.dummy.services;
 
-import com.hellodoctor.dummy.models.Student;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
