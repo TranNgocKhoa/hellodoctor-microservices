6c1ffe9c4214ee26e8e5b1ba487b58195a0f1b06
tranngockhoa <tranngoc.khoa.spk@gmail.com>
2020-05-25 00:26:16 +0700

    migrate to spring cloud api gateway

--- dummy/src/main/java/com/hellodoctor/dummy/models/Student.java
+++
@@ -1,9 +0,0 @@
-package com.hellodoctor.dummy.models;
-
-import lombok.Data;
-
-@Data(staticConstructor = "of")
-public class Student {
-    private String name;
-    private int yearOfBirth;
-}
--- gateway/src/main/java/com/hellodoctor/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/hellodoctor/gateway/GatewayApplication.java
@@ -3,15 +3,12 @@ package com.hellodoctor.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulServer
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
 }
